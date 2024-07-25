b1b4518caf23c8b0c17e30aa17bc538f0a6a963b
Rob Tipping <61379257+RobTipping@users.noreply.github.com>
2024-07-25 21:42:34 +0100

    made the ring

--- src/main.c
+++ src/main.c
@@ -1,31 +1,44 @@
 #include "raylib.h"
 
+#define MAX_SPEED 100.0f
+
 typedef struct sumo {
   Vector2 pos;
   float r;
   Color color;
 } sumo;
 
+typedef struct ring {
+  Vector2 pos;
+  float r;
+  Color color;
+} ring;
+
 sumo playerOne;
 sumo playerTwo;
+ring dojo;
 
 void updatePlayer();
 
 int main(void) {
   InitWindow(800, 800, "Sumo Prime Time!");
   SetTargetFPS(60);
-  playerOne.pos = (Vector2){50, 50};
+  playerOne.pos = (Vector2){200, 400};
   playerOne.r = 25.0;
   playerOne.color = GREEN;
-  playerTwo.pos = (Vector2){750, 50};
+  playerTwo.pos = (Vector2){600, 300};
   playerTwo.r = 25.0;
   playerTwo.color = BLUE;
+  dojo.pos = (Vector2){400, 400};
+  dojo.r = 300.0;
+  dojo.color = RED;
 
   while (!WindowShouldClose()) {
     updatePlayer();
     // draw shit
     BeginDrawing();
     ClearBackground(GRAY);
+    DrawCircleV(dojo.pos, dojo.r, dojo.color);
     DrawCircleV(playerOne.pos, playerOne.r, playerOne.color);
     DrawCircleV(playerTwo.pos, playerTwo.r, playerTwo.color);
     EndDrawing();
@@ -36,8 +49,10 @@ int main(void) {
 
 void updatePlayer() {
   float deltaTime = GetFrameTime();
-  if (!CheckCollisionCircles(playerOne.pos, playerOne.r, playerTwo.pos, playerTwo.r)) {
-    playerOne.pos.x += 20.0 * deltaTime;
-    playerTwo.pos.x -= 20.0 * deltaTime;
+  if (CheckCollisionCircles(playerOne.pos, playerOne.r, dojo.pos, dojo.r - (playerOne.r * 2.0))) {
+    if (!CheckCollisionCircles(playerOne.pos, playerOne.r, playerTwo.pos, playerTwo.r)) {
+      playerOne.pos.x += MAX_SPEED * deltaTime;
+      playerTwo.pos.x -= MAX_SPEED * deltaTime;
+    }
   }
 }
